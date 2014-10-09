517c481a2297b4eb4323dccb76c1ac6baa9d190c
horica007 <horica007@gmail.com>
2014-10-09 11:24:25 -0400

    Adding scripts

---
+++ Assets/While.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class While : MonoBehaviour {
+
+	int cupsInTheSink = 4;
+
+	//Debug.Log ("Hello!");
+
+	// Use this for initialization
+	void Start () 
+	{
+		if (cupsInTheSink == 4)
+			Debug.Log ("Am I getting a hang of this?");
+
+		while (cupsInTheSink > 0) 
+		{
+			Debug.Log ("I've washed a cup!");
+			cupsInTheSink--;
+		}
+		if (cupsInTheSink == 0)
+			Debug.Log ("Maybe i can do this");
+	}
+
+	/*
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	*/
+}
